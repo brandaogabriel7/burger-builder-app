bd1555986515d113a9181b7dede6d337a042869a
Gabriel <gabriel.silva.brandao7@gmail.com>
2021-04-02 15:29:20 -0300

    feat: setup initial project settings and tree

---
+++ src/pages/App/App.test.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('app should', () => {
+    it('render correctly', () => {
+        render(<App />);
+    });
+});
