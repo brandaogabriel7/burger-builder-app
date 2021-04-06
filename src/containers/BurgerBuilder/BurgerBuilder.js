6c5435147fa106fa4ab22e6e7fc2e48e001ed69c
Gabriel Silva Brandão <gabriel.silva.brandao7@gmail.com>
2021-04-06 17:25:42 -0300

    feat: implement hoc for axios error handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 
 import ordersApi from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -94,6 +95,7 @@ class BurgerBuilder extends Component {
             })
             .catch(err => {
                 this.setState({ loading: false, purchasing: false });
+                console.log(err);
             });
     };
 
@@ -132,4 +134,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, ordersApi);
